495c0b0284d40f07406735f954000d9d6d5ebd28
Beulah Mathenge <beulahmathenge@gmail.com>
2022-08-05 16:58:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+document.querySelector('main');
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+document.body.append(newHeader);
+
+newHeader.setAttribute('id','h1');
+
+newHeader.id='victory';
+
+document.getElementById("victory");
+
+newHeader.innerHTML =" Daizy is the champion";
+
